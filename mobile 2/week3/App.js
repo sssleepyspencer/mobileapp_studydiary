3622cec39b45c72081d1de4f7c9174a42c7cdda2
Miaomiao Kang <146165286+sssleepyspencer@users.noreply.github.com>
2025-09-27 02:38:26 +0300

    Create App.js

---
+++ mobile 2/week3/App.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import HomeScreen from './screens/HomeScreen';
+import ForecastScreen from './screens/ForecastScreen';
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Forecast" component={ForecastScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
